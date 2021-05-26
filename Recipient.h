815ba6f7c3b9669ea42be7451cd6dc33addc3df4
Anas-Abd-ElAziz <mohamedanas709@gmail.com>
2021-05-26 20:55:52 +0200

    Added vars & functions to Recipient.h

--- Recipient.h
+++ Recipient.h
@@ -1,5 +1,28 @@
-#pragma once
+#include <string>
+
+using namespace std;
 class Recipient
 {
+public:
+	//variables
+	int id;
+	string name;
+	string mail;
+	int password;
+	int age;
+	string gender;
+	string blood_type;
+	string hospital;
+	string doc_of_case;
+	//functions
+	Recipient();
+	void reg(); // register
+	void login();
+	void updateAccount();
+	void deleteAccount();
+	bool isBloodAvailable();
+	void displayBloodData();
+	void requestAndConfirm();
+	~Recipient();
 };
 
